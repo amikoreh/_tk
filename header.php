34d84e7fe20c2d4b3c570e7d3bc5bae441357731
Ami Koreh <korehaz@gmail.com>
2016-01-09 10:59:54 -0800

    Update header.php

--- header.php
+++ header.php
@@ -52,7 +52,7 @@
 <?php // substitute the class "container-fluid" below if you want a wider content area ?>
 	<div class="container">
 		<div class="row">
-			<div class="site-navigation-inner col-sm-12">
+			<div class="site-navigation-inner col-xs-12">
 				<div class="navbar navbar-default">
 					<div class="navbar-header">
 						<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
@@ -91,5 +91,5 @@
 <?php // substitute the class "container-fluid" below if you want a wider content area ?>
 	<div class="container">
 		<div class="row">
-			<div id="content" class="main-content-inner col-sm-12 col-md-8">
+			<div id="content" class="main-content-inner col-xs-12 col-md-8">
 
